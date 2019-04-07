7fd985743fc09d9842f044b0c60b2d8fcb564d9e
Alan Campora <acampora@medallia.com>
2019-04-07 01:14:29 -0300

    refactor(style): modify margin and p font size

--- src/templates/styled.js
+++ src/templates/styled.js
@@ -1,14 +1,19 @@
 import styled from 'styled-components';
 
 export const StyledWrapper = styled.div`
-  background-color: #f8f8f8;
+	background-color: #f8f8f8;
 	padding: 1rem;
+	margin: 0 15rem;
 
-	h1, h2, h3{
+	h1,
+	h2,
+	h3 {
 		font-weight: 400;
 		margin: 1rem;
 	}
+
 	p {
 		margin: 1rem 5%;
+		font-size: 1.2rem;
 	}
 `;
