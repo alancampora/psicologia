957e87557ffcfc0a0b92e28c2e57c433ea7d607c
Alan Campora <acampora@medallia.com>
2019-04-07 20:10:12 -0300

    refactor(style): fix styles table h and li

--- src/templates/styled.js
+++ src/templates/styled.js
@@ -1,23 +1,35 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const StyledWrapper = styled.div`
-	background-color: white;
-	padding: 1rem;
-	margin: 0 10%;
+  background-color: white;
+  padding: 1rem;
+  margin: 0 10%;
 
-	h1,
-	h2,
-	h3 {
-		font-weight: 400;
-		margin: 1rem;
-	}
+  h1,
+  h2,
+  h3 {
+    font-weight: 600;
+    margin: 2rem 1rem;
+  }
 
-	p {
-		margin: 1rem 5%;
-		font-size: 1.2rem;
-	}
+  p {
+    margin: 1rem 5%;
+    font-size: 1.2rem;
+  }
+
+  ul {
+    margin-top: 1.5rem;
+    margin-left: 6rem;
+    font-size: 1.1rem;
+  }
+
+  table {
+    font-size: 1.2rem;
+    width: 90%;
+    margin: 0 auto;
+  }
 `;
 
 export const StyledBody = styled.div`
-	background-color: #ff5b5b;
+  background-color: #ff5b5b;
 `;
